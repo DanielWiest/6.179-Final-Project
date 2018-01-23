a30a1dc6fe3bfa9bfc3c8c6e5103e33a7a1e17ac
DanielWiest <35710377+DanielWiest@users.noreply.github.com>
2018-01-22 21:02:27 -0500

    Nonfunctional, still work in progress. Saving as backup.

--- main.cpp
+++ main.cpp
@@ -1,6 +1,26 @@
 #include <iostream>
+#include <cmath>
 
-int main() {
+const long gravitationalConst = 6.67408*pow(10,-11); //
+const double timestep = 10; //In seconds
 
-   return 0;
+class plnt_obj {
+public:
+    double x,y,z,vx,vy,vz,mass;
+    plnt_obj(double xPos,double yPos,double zPos, double xVel, double yVel, double zVel,double massInp):
+        x(xPos),y(yPos),z(zPos),vx(xVel),vy(yVel),vz(zVel),mass(massInp){};
+};
+
+double distComp(plnt_obj planet1,plnt_obj planet2) {
+    return pow( (pow((planet2.x-planet1.x),2)+pow((planet2.y-planet1.y),2)+pow((planet2.z-planet1.z),2)), 0.5);
+}
+
+double forceBetweenPlanets(plnt_obj planet1,plnt_obj planet2) {
+    return (gravitationalConst*planet1.mass*planet2.mass)/distComp(planet1,planet2);
+}
+
+
+
+int main(int argc, const char * argv[]) {
+
 }
