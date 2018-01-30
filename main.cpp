ae4c9985dbd0c56425d23859fa0fc908471544e3
DanielWiest <35710377+DanielWiest@users.noreply.github.com>
2018-01-30 17:04:56 -0500

    Input is functional

--- main.cpp
+++ main.cpp
@@ -1,15 +1,35 @@
 #include <iostream>
+#include <fstream>
 #include <cmath>
+#include <map>
+#include <vector>
+#include <algorithm>
+#include <math.h>
+#include <string>
+
 
 const long gravitationalConst = 6.67408*pow(10,-11); //
-const double timestep = 10; //In seconds
+//const double timestep = 10; //In second
 
 class plnt_obj {
 public:
-    double x,y,z,vx,vy,vz,mass;
+    std::string name;
+    long time;
+    long x,y,z,vx,vy,vz,mass;
+    plnt_obj(std::string name):
+        name(name),x(0),y(0),z(0),vx(0),vy(0),vz(0),mass(0){};
     plnt_obj(double xPos,double yPos,double zPos, double xVel, double yVel, double zVel,double massInp):
-        x(xPos),y(yPos),z(zPos),vx(xVel),vy(yVel),vz(zVel),mass(massInp){};
+        name(""), x(xPos),y(yPos),z(zPos),vx(xVel),vy(yVel),vz(zVel),mass(massInp){};
+    plnt_obj():
+        name(""),x(0),y(0),z(0),vx(0),vy(0),vz(0),mass(0){};
 };
+
+class SolarSystem {
+public:
+    std::vector<plnt_obj> planetVector;
+    
+};
+
 
 double distComp(plnt_obj planet1,plnt_obj planet2) {
     return pow( (pow((planet2.x-planet1.x),2)+pow((planet2.y-planet1.y),2)+pow((planet2.z-planet1.z),2)), 0.5);
@@ -21,6 +41,69 @@ double forceBetweenPlanets(plnt_obj planet1,plnt_obj planet2) {
 
 
 
-int main(int argc, const char * argv[]) {
 
+int main(int argc, const char * argv[]) {
+    int planetCount;
+    int count;
+    SolarSystem currentSys;
+    std::ifstream ifs;
+    ifs.open ("POI.txt", std::ifstream::in);
+    std::string value;
+    while ( ifs.good() ){
+        
+        getline ( ifs, value, ',' ); // read a string until next comma:
+        std::cout << value<<std::endl;
+        switch (count) {
+            case 0:
+                if (value == "") {
+                    
+                }
+                else {
+                    std::cout << "NAME\n";
+                    currentSys.planetVector.push_back(plnt_obj(value));
+                }
+                break;
+            case 1:
+                std::cout << "Time\n";
+                currentSys.planetVector[planetCount].time = stol(value);
+                break;
+            case 2:
+                std::cout << "Mass\n";
+                currentSys.planetVector[planetCount].mass = stol(value);
+                break;
+            case 3:
+                std::cout << "x\n";
+                currentSys.planetVector[planetCount].x = stol(value);
+                break;
+            case 4:
+                std::cout << "y\n";
+                currentSys.planetVector[planetCount].y = stol(value);
+                break;
+            case 5:
+                std::cout << "z\n";
+                currentSys.planetVector[planetCount].z = stol(value);
+                break;
+            case 6:
+                std::cout << "vx\n";
+                currentSys.planetVector[planetCount].vx = stol(value);
+                break;
+            case 7:
+                std::cout << "vy\n";
+                currentSys.planetVector[planetCount].vy = stol(value);
+                break;
+            case 8:
+                std::cout << "vz\n";
+                currentSys.planetVector[planetCount].vz = stol(value);
+                planetCount++;
+                count = -1;
+                break;
+        }
+        count++;
+    }
+    for (std::vector<plnt_obj>::iterator i = planetVector.begin(); i != planetVector.end(); i++){
+        std::cout << (*i).name << std::endl;
+    }
+    
+    
+    
 }
